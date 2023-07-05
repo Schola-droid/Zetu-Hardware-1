c02dcbfea56a679c09c05083c0e3f54c50b37d7e
Ngare <45742522+Ngaremaina@users.noreply.github.com>
2023-07-05 23:00:52 +0300

    Styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
         <nav>
             <NavLink to="/hardware">Home</NavLink>
             <NavLink>Logout</NavLink>
-            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/login" >Login</NavLink>
             <NavLink to="/signup">SignUp</NavLink>
         </nav>
     )
